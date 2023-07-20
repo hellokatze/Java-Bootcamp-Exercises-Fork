e9c18ec0eb964995c1dc5d7830cf952a5ca1a00f
Katharine Lin <katharine.exe@gmail.com>
2023-07-20 11:12:17 +0200

    add randomWord generator and print placeholder underscores

--- Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
+++ Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
@@ -1,10 +1,12 @@
 public class Hangman {
+  // 1. write function that will return random word from words array using Math.random + index
+  // 2. display 1 '_' for each character in the selected word
 
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
     "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
     "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
     "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
-    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
+    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon",
     "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
     "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
     "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
@@ -61,23 +63,33 @@ public class Hangman {
     " +---+\n" +
     " |   |\n" +
     " O   |\n" +
-    "/|\\  |\n" + 
+    "/|\\  |\n" +
     "/ \\  |\n" +
     "     |\n" +
     " =========\n"};
 
     public static void main(String[] args) {
-
-
-
-
+      String randomWord = randomWord(words);
+      char[] characters = placeholders(randomWord);
+      System.out.print("Word:\t");
+      for (int i = 0; i < characters.length; i++) {
+        System.out.print(characters[i] + " ");
+      }
 
 
     }
 
+    public static String randomWord(String[] words) {
+      int randomNumber = (int) (Math.random() * words.length);
+      return words[randomNumber];
+    }
+
+    public static char[] placeholders(String word) {
+      char[] characters = new char[word.length()];
+      for (int i = 0; i < characters.length; i++) {
+        characters[i] = '_';
+      }
+      return characters;
+    }
+
 }
-
-
-
-
-
