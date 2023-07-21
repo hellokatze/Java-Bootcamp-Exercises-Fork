50e58f45fa3967d23b6662b1ce717306ed3de12f
Katharine Lin <katharine.exe@gmail.com>
2023-07-21 10:54:57 +0200

    add constructor to created object

--- Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.2/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.2/Main.java
@@ -1,23 +1,15 @@
 import java.util.Arrays;
- 
+
 public class Main {
-  
+
     public static void main(String[] args) {
-        Person person = new Person();
- 
-        person.name = "Rayan Slim";
-        person.nationality = "Canadian";
-        person.dateOfBirth = "01/01/1111";
+        Person person = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
+
         person.passport = new String[]{person.name, person.nationality, person.dateOfBirth};
-        person.seatNumber = 5;
-        
-        System.out.println(person.name);
-        System.out.println(person.nationality);
-        System.out.println(person.dateOfBirth);
-        System.out.println(Arrays.toString(person.passport));
-        System.out.println(person.seatNumber);
+
+        System.out.println("Name: " + person.name + "\n" + "Nationality: " + person.nationality + "\n" + "Date of birth: " + person.dateOfBirth + "\n" + "Seat number: " + person.seatNumber);
 
     }
-  
-  
+
+
 }
