a2f4ef4d2c3a63de4219530ece2d863180c806b9
Katharine Lin <katharine.exe@gmail.com>
2023-07-21 10:45:09 +0200

    add constructor to Car class

--- Module 2 - Object Oriented Programming/7. The World of Objects/section-seven/Car.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/section-seven/Car.java
@@ -3,6 +3,13 @@ public class Car {
   double price;
   int year;
   String color;
+
+  public Car(String make, double price, int year, String color) {
+    this.make = make; // the make parameter in the constructor refers to the object field make.
+    this.price = price;
+    this.year = year;
+    this.color = color;
+  }
 }
 
 // A class is a blueprint that can create many car objects.
--- Module 2 - Object Oriented Programming/7. The World of Objects/section-seven/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/section-seven/Main.java
@@ -1,16 +1,16 @@
 public class Main {
   public static void main(String[] args) {
-    Car nissan = new Car();
-    nissan.make = "Nissan";
-    nissan.price = 10000;
-    nissan.year = 2020;
-    nissan.color = "green";
+    Car nissan = new Car("Nissan", 10000, 2020, "green");
+    // nissan.make = "Nissan";
+    // nissan.price = 10000;
+    // nissan.year = 2020;
+    // nissan.color = "green";
 
-    Car dodge = new Car();
-    dodge.make = "Dodge";
-    dodge.price = 11000;
-    dodge.year = 2019;
-    dodge.color = "blue";
+    Car dodge = new Car("Dodge", 11000, 2019, "blue");
+    // dodge.make = "Dodge";
+    // dodge.price = 11000;
+    // dodge.year = 2019;
+    // dodge.color = "blue";
 
     System.out.println("This " + nissan.make + " is worth $" + nissan.price +
     ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
