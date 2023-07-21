3808dd59cd299a22da4d81986dae8c061803fdd7
Katharine Lin <katharine.exe@gmail.com>
2023-07-21 10:15:06 +0200

    create new Person object

--- Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
@@ -1,3 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
+  public static void main(String[] args) {
     
+    Person henry = new Person();
+    henry.name = "Henry";
+    henry.nationality = "Romanian";
+    henry.dateOfBirth = "05/06/2021";
+    henry.passport = new String[]{henry.name, henry.nationality, henry.dateOfBirth};
+    henry.seatNumber = 3;
+
+    System.out.println(henry.name);
+    System.out.println(henry.nationality);
+    System.out.println(henry.dateOfBirth);
+    System.out.println(Arrays.toString(henry.passport));
+    System.out.println(henry.seatNumber);
+
+  }
 }
