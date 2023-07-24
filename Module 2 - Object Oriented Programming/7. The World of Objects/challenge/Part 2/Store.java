616f8a60c563dd4140afb6649c8302d42c17e34f
Katharine Lin <katharine.exe@gmail.com>
2023-07-24 17:07:08 +0200

    add getter and setter, populate store

--- Module 2 - Object Oriented Programming/7. The World of Objects/challenge/Part 2/Store.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/challenge/Part 2/Store.java
@@ -1,18 +1,24 @@
 public class Store {
 
-    private Movie[] movies;
+    private Movie[] movies; // an array that stores movie objects
 
     public Store() {
         // TODO
+        this.movies = new Movie[10];
     }
 
+    // getMovie() and setMovie() methods are for populating the Movie[10] array initialized upon creation of a new store.
+
     public Movie getMovie(int index) {
-        // TODO 
-        return null;
+        // TODO
+        Movie copy = this.movies[index];
+        return new Movie(copy);
     }
 
     public void setMovie(int index, Movie movie) {
-        // TODO 
+        // TODO
+        Movie copy = new Movie(movie);
+        this.movies[index] = copy;
     }
 
 
