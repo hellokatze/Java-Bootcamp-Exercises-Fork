745335a8d537a0503447f771d66a06c42eb0d605
Katharine Lin <katharine.exe@gmail.com>
2023-07-24 15:36:47 +0200

    finalize dealership app

--- Module 2 - Object Oriented Programming/7. The World of Objects/section-seven/Dealership.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/section-seven/Dealership.java
@@ -12,6 +12,16 @@ public class Dealership {
     this.cars = Arrays.copyOf(cars, cars.length);
   }
 
+  public Car getCar(int index) {
+    Car copy = new Car(this.cars[index]);
+    return copy;
+  }
+
+  public void setCar(int index, Car newCar) {
+    Car copy = new Car(newCar); // creates a copy of newCar
+    this.cars[index] = copy;
+  }
+
   public void sell(int index) {
     this.cars[index].drive();
   }
