ce4506541e970cf309c7b3e0f356d641b5a0c2ca
Katharine Lin <katharine.exe@gmail.com>
2023-07-27 17:24:21 +0200

    set argument validations

--- Module 2 - Object Oriented Programming/10. Exception Handling/Challenge/Part 1/Movie.java
+++ Module 2 - Object Oriented Programming/10. Exception Handling/Challenge/Part 1/Movie.java
@@ -1,5 +1,5 @@
 public class Movie {
-    
+
     private String name;
     private String format;
     private double rating;
@@ -22,15 +22,21 @@ public class Movie {
 
     public void setName(String name) {
         // TODO
+        if (name.isBlank() || name == null) {
+          throw new IllegalArgumentException("Name can't be blank or null!");
+        }
         this.name = name;
     }
-    
+
     public String getFormat() {
         return format;
     }
 
     public void setFormat(String format) {
         // TODO
+        if (format.isBlank() || format == null) {
+          throw new IllegalArgumentException("Format can't be blank or null!");
+        }
         this.format = format;
     }
 
@@ -40,11 +46,14 @@ public class Movie {
 
     public void setRating(double rating) {
         // TODO
+        if (rating < 0 && rating > 10) {
+          throw new IllegalArgumentException("Please indicate a rating between 0 and 10.");
+        }
         this.rating = rating;
     }
 
     public String toString() {
         return this.rating + "\t" + this.format + "\t\t" + this.name + "";
-    }    
+    }
 
 }
