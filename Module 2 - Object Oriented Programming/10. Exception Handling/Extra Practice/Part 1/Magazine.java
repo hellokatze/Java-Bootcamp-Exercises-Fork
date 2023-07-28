ad5834e0047d817bd52ff31d47b3dfc7abe1513e
Katharine Lin <katharine.exe@gmail.com>
2023-07-28 09:12:20 +0200

    add setters with IllegalArgumentExceptions

--- Module 2 - Object Oriented Programming/10. Exception Handling/Extra Practice/Part 1/Magazine.java
+++ Module 2 - Object Oriented Programming/10. Exception Handling/Extra Practice/Part 1/Magazine.java
@@ -36,5 +36,33 @@ public class Magazine {
   public int getPublicationYear() {
     return this.publicationYear;
   }
-  
+
+  // Setters
+  public void setTitle(String newTitle) {
+    if (newTitle == null || newTitle.isBlank()) {
+      throw new IllegalArgumentException("Title can't be null or blank.");
+    }
+    this.title = newTitle;
+  }
+
+  public void setPublisher(String newPublisher) {
+    if (newPublisher == null || newPublisher.isBlank()) {
+      throw new IllegalArgumentException("Publisher can't be null or blank.");
+    }
+    this.publisher = newPublisher;
+  }
+
+  public void setIssueNumber(int newIssueNumber) {
+    if (newIssueNumber <= 0) {
+      throw new IllegalArgumentException("Issue number must be greater than 1.");
+    }
+    this.issueNumber = newIssueNumber;
+  }
+
+  public void setPublicationYear(int newPublicationYear) {
+    if (newPublicationYear <= 0) {
+      throw new IllegalArgumentException("Publication year must be greater than 0");
+    }
+    this.publicationYear = newPublicationYear;
+  }
 }
