faed2d3d14a942c92253576ff9e7d40e7983347e
Katharine Lin <katharine.exe@gmail.com>
2023-07-31 12:13:47 +0200

    correct setCity method

--- Module 2 - Object Oriented Programming/11. Map Collections/Extra Practice/Part 2/CityPopulationTracker.java
+++ Module 2 - Object Oriented Programming/11. Map Collections/Extra Practice/Part 2/CityPopulationTracker.java
@@ -14,9 +14,16 @@ public class CityPopulationTracker {
   }
 
   public void setCity(City newCity) {
-    City city = this.cityPopulations.get(newCity);
-    City copy = new City(city.getName(), city.getCountry(), city.getPopulation());
-    this.cityPopulations.put(city.getName(), copy);
+    City copy = new City(newCity.getName(), newCity.getCountry(), newCity.getPopulation());
+    if (this.cityPopulations.containsKey(newCity.getName())) {
+      this.cityPopulations.put(newCity.getName(), copy);
+    } else {
+      addCity(copy);
+    }
   }
 
+  public void addCity(City newCity) {
+    City copy = new City(newCity.getName(), newCity.getCountry(), newCity.getPopulation());
+    this.cityPopulations.put(copy.getName(), copy);
+  }
 }
