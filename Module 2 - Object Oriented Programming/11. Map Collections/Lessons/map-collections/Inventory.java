a4d68990ae0d289bd75769b5fd47138426f37b32
Katharine Lin <katharine.exe@gmail.com>
2023-07-31 10:41:32 +0200

    create HashMap and TreeMap

--- Module 2 - Object Oriented Programming/11. Map Collections/Lessons/map-collections/Inventory.java
+++ Module 2 - Object Oriented Programming/11. Map Collections/Lessons/map-collections/Inventory.java
@@ -1,32 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inventory {
     public static void main(String[] args) {
 
-        // inventory.put("Bananas", 2.99);
-        // inventory.put("Papaya", 5.99);
-        // inventory.put("Kale", 4.99);
-        // inventory.put("Apples", 1.99);
-        // inventory.put("Carrot", 1.49);
-        
-        // inventory.put("Lime", 0.49);
-        // inventory.put("Toothpaste", 5.99);
-        // inventory.put("Zucchini", 5.99);
+        Map<String, Double> inventory = new HashMap<>();
+        inventory.put("Bananas", 2.99); // any collection of type Map can call put method, which accepts key-value pair.
+        inventory.put("Papaya", 5.99); // with addition of first key-value pair, a 16-element array with null gets created.
+        inventory.put("Kale", 4.99); // a function processes key and returns integer hash numerical value.
+        inventory.put("Apples", 1.99); // bitwise operation on the hash value determines index on where key-value pair is going to be stored.
+        inventory.put("Carrot", 1.49); // element at the index stores reference to a node, which stores key-value entry.
 
-        // System.out.println("\nWelcome to Java Grocers. What can we help you find?:\n");
+        inventory.put("Lime", 0.49);
+        inventory.put("Toothpaste", 5.99);
+        inventory.put("Zucchini", 5.99);
 
-        // System.out.println("\nWhat is the price of Lime?");
-        // System.out.println(inventory.get("Lime"));
+        System.out.println("\nWelcome to Java Grocers. What can we help you find?:\n");
 
-        // System.out.println("\nWhat is the price of Zucchini?");
-        // System.out.println(inventory.get("Zucchini"));
+        System.out.println("\nWhat is the price of Lime?");
+        System.out.println(inventory.get("Lime"));
 
-        // System.out.println("\nWhat is the price of Kale?");
-        // System.out.println(inventory.get("Kale"));
+        System.out.println("\nWhat is the price of Zucchini?");
+        System.out.println(inventory.get("Zucchini"));
+
+        System.out.println("\nWhat is the price of Kale?");
+        System.out.println(inventory.get("Kale"));
 
 
-        // System.out.println("\nDo you guys sell Apples?");
-        // boolean result = inventory.containsKey("Apples");
-        // String response = result ? "Yes!" : "No, sorry!";
-        // System.out.println(response);
+        System.out.println("\nDo you guys sell Apples?");
+        boolean result = inventory.containsKey("Apples");
+        String response = result ? "Yes!" : "No, sorry!";
+        System.out.println(response);
 
     }
 }
