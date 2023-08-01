b502885f1768e2d91ccf94a92847b34667b1a0f9
Katharine Lin <katharine.exe@gmail.com>
2023-08-01 12:44:17 +0200

    test methods

--- Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 1/Main.java
+++ Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 1/Main.java
@@ -1,7 +1,38 @@
-import model.Car.BodyType.*;
+import model.Car;
+import static model.Car.BodyType.*; // static import of every field inside of BodyType enum
 
 public class Main {
     public static void main(String[] args) {
+        Car car1 = new Car("Toyota", "Camry", SEDAN, 2020, 30000);
+        System.out.println(car1.getMake());
+
+
+    // Uncomment the following line to test invalid input for setMake method
+    car1.setMake("Nissan");
+
+
+    // Uncomment the following line to test invalid input for setModel method
+    car1.setModel("Ultra");
+
+
+    // Uncomment the following line to test invalid input for setBodyType method
+    car1.setBodyType(SUV);
+
+
+    // Uncomment the following line to test invalid input for setProductionYear method
+    car1.setProductionYear(2022);
+
+
+    // Uncomment the following line to test invalid input for setPrice method
+    car1.setPrice(2000);
+
+
+    // Uncomment the following line to test invalid input for setPrice method
+    car1.setPrice(20000);
+
+
+    // Uncomment the following line to test invalid input for the constructor
+    Car car2 = new Car("Toyota", "Camry", SEDAN, 2020, 30000);
 
     }
 }
--- Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 1/model/Car.java
+++ Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 1/model/Car.java
@@ -8,27 +8,30 @@ public class Car {
   private double price;
 
   public enum BodyType {
-    SEDAN, COUPE, HATCHBACK, SUV, TRUCK, VAN;
+    SEDAN, COUPE, HATCHBACK, SUV, TRUCK, VAN
   }
 
   public static final int MIN_PROD_YEAR = 1900;
   public static final double MIN_PRICE = 0;
   public static final double MAX_PRICE = 200000;
 
+  // in the scope of the constructor, no need to use this.make since the object and its fields have not been initialized yet and there is no confusion.
   public Car(String make, String model, BodyType bodyType, int productionYear, double price) {
-    setMake(this.make);
-    setModel(this.model);
-    setBodyType(this.bodyType);
-    setProductionYear(this.productionYear);
-    setPrice(this.price);
+    setMake(make);
+    setModel(model);
+    setBodyType(bodyType);
+    setProductionYear(productionYear);
+    setPrice(price);
   }
 
   public String getMake() {
-    return this.make;
+    return make;
   }
 
   public void setMake(String make) {
-    if (make == null || make.isBlank()) throw new IllegalArgumentException("Make can't be null or blank.");
+    if (make == null || make.isBlank()) {
+      throw new IllegalArgumentException("Make can't be null or blank.");
+    }
     this.make = make;
   }
 
@@ -37,7 +40,9 @@ public class Car {
   }
 
   public void setModel(String model) {
-    if (model == null || model.isBlank()) throw new IllegalArgumentException("Model can't be null or blank.");
+    if (model == null || model.isBlank()) {
+      throw new IllegalArgumentException("Model can't be null or blank.");
+    }
     this.model = model;
   }
 
@@ -57,7 +62,9 @@ public class Car {
   }
 
   public void setProductionYear(int year) {
-    if (year < MIN_PROD_YEAR) throw new IllegalArgumentException("Production year must be greater than or equal to " + MIN_PROD_YEAR + ".");
+    if (year < MIN_PROD_YEAR) {
+      throw new IllegalArgumentException("Production year must be greater than or equal to " + MIN_PROD_YEAR + ".");
+    }
     this.productionYear = year;
   }
 
@@ -66,7 +73,10 @@ public class Car {
   }
 
   public void setPrice(double price) {
-    if (price < MIN_PRICE || price > MAX_PRICE) throw new IllegalArgumentException("Price must be between " + MIN_PRICE + " and " + MAX_PRICE + ".");
+    if (price < MIN_PRICE || price > MAX_PRICE) {
+      throw new IllegalArgumentException("Price must be between " + MIN_PRICE + " and " + MAX_PRICE + ".");
+    }
+    this.price = price;
   }
 
 }
