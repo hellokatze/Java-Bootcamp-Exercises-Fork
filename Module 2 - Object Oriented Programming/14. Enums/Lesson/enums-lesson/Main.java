237708dddcfd424dc053bf54377544e87fc08f48
Katharine Lin <katharine.exe@gmail.com>
2023-08-01 11:36:55 +0200

    refactor with enums

--- Module 2 - Object Oriented Programming/14. Enums/Lesson/enums-lesson/Main.java
+++ Module 2 - Object Oriented Programming/14. Enums/Lesson/enums-lesson/Main.java
@@ -1,10 +1,11 @@
 import model.Car;
+import static model.Car.TrafficLight.*; // allows us to use TrafficLight enum directly without having to write out class name.
 
 public class Main {
 
     public static void main(String[] args) {
         Car car = new Car("Nissan", 2020);
-        car.drive("RED");
+        car.drive(RED);
     }
 
 }
