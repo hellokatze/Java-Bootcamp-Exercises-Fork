43eab7b3f8adea3d537bad61dcb93b5b22edfa6f
Katharine Lin <katharine.exe@gmail.com>
2023-08-01 13:09:47 +0200

    add validation methods to main

--- Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 3/Main.java
+++ Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 3/Main.java
@@ -1,5 +1,29 @@
+import static model.Car.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static boolean isNullOrBlank(String input) {
+      return input == null || input.isBlank();
+    }
+
+    public static boolean invalidYear(int year) {
+      return year < MIN_YEAR;
+    }
+
+    public static boolean invalidPrice(double price) {
+      return price < MIN_PRICE || price > MAX_PRICE;
+    }
+
+    public static boolean invalidBodyType(String string) {
+      String capitalized = string.toUpperCase();
+      try {
+        BodyType.valueOf(capitalized);
+      } catch (IllegalArgumentException e) {
+        System.out.println("Invalid body type: " + capitalized);
+      }
+      return true;
     }
 }
