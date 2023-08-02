7bc725338169dfedd7b74d06c75b31be2a745a66
Katharine Lin <katharine.exe@gmail.com>
2023-08-02 12:29:22 +0200

    add copy constructors

--- Module 2 - Object Oriented Programming/15. Inheritance/Extra Practice/Part 2/Main.java
+++ Module 2 - Object Oriented Programming/15. Inheritance/Extra Practice/Part 2/Main.java
@@ -1,5 +1,20 @@
+import model.Centrifuge;
+import model.Microscope;
+
 public class Main {
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
+    Microscope microscope1 = new Microscope("Nikon", "E200", 2021, 1000);
+    Centrifuge centrifuge1 = new Centrifuge("Eppendorf", "5424R", 2020, 15000);
+
+    // Test the copy constructors
+    Microscope microscopeCopy = new Microscope(microscope1);
+    Centrifuge centrifugeCopy = new Centrifuge(centrifuge1);
+
+    System.out.println("Original Microscope: " + microscope1.getManufacturer() + " " + microscope1.getModel());
+    System.out.println("Copied Microscope: " + microscopeCopy.getManufacturer() + " " + microscopeCopy.getModel());
+
+    System.out.println("Original Centrifuge: " + centrifuge1.getManufacturer() + " " + centrifuge1.getModel());
+    System.out.println("Copied Centrifuge: " + centrifugeCopy.getManufacturer() + " " + centrifugeCopy.getModel());
+
+  }
 }
--- Module 2 - Object Oriented Programming/15. Inheritance/Extra Practice/Part 2/model/Lab.java
+++ Module 2 - Object Oriented Programming/15. Inheritance/Extra Practice/Part 2/model/Lab.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Lab {
-    
+
+  private ArrayList<LabEquipment> labEquipments;
+
+  public Lab() {
+    this.labEquipments = new ArrayList<>();
+  }
+
+  public Lab(Lab source) {
+    this.labEquipments = new ArrayList<>(source.labEquipments);
+  }
+
 }
