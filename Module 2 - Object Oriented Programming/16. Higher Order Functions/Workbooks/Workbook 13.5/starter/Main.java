3e3a1d37e282f797e5ad39b975a22a8f76413e35
Katharine Lin <katharine.exe@gmail.com>
2023-08-11 12:34:58 +0200

    add filter, map, and sorted intermediate operations

--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Workbooks/Workbook 13.5/starter/Main.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Workbooks/Workbook 13.5/starter/Main.java
@@ -24,6 +24,9 @@ public class Main {
         System.out.println("\nThese books are on sale\n-----------------------");
         books.stream()
             // intermediate operations go here...
-            .forEach(null); // terminal operation
+            .filter(book -> book.getGenre().equals("Romance"))
+            .map(book -> new Book(book.getTitle(), book.getGenre(), book.getYear(), book.getPrice()/2))
+            .sorted((x, y) -> x.getYear().compareTo(y.getYear()))
+            .forEach(book -> System.out.println(book.getGenre() + "\t" + book.getYear() + "\t" + book.getPrice() + "\t" + book.getTitle())); // terminal operation
     }
 }
