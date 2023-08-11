63701d46aa8a483e350160247f141d71aeb9b33d
Katharine Lin <katharine.exe@gmail.com>
2023-08-11 14:38:55 +0200

    add getters to Movie class

--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/Movie.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/Movie.java
@@ -2,4 +2,34 @@ package model;
 
 public class Movie {
 
+  private String title;
+  private String director;
+  private String genre;
+  private int year;
+  private double rating;
+
+  public Movie(String title, String director, String genre, int year, double rating) {
+
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public String getDirector() {
+    return this.director;
+  }
+
+  public String getGenre() {
+    return this.genre;
+  }
+
+  public int getYear() {
+    return this.year;
+  }
+
+  public double getRating() {
+    return this.rating;
+  }
+
 }
