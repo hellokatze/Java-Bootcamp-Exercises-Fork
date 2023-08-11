9ae187ebcca9e2c1e56bbca4c81a8fabdab9f7e9
Katharine Lin <katharine.exe@gmail.com>
2023-08-11 14:20:10 +0200

    add constructor and addMovie methods

--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/MovieStore.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/MovieStore.java
@@ -1,5 +1,20 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieStore {
+
+  private List<Movie> movies;
+
+  public MovieStore() {
+    this.movies = new ArrayList<>();
+  }
+
+  public void addMovie(Movie movie) {
+    this.movies.add(movie);
+  }
+
+
 
 }
