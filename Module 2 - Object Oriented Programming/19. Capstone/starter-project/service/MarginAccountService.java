17b8947bc06aceb823f2a6d66c16cd76036f9bf0
Katharine Lin <katharine.exe@gmail.com>
2023-08-14 15:48:46 +0200

    add CRUD and Override methods to MarginAccountService

--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/service/MarginAccountService.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/service/MarginAccountService.java
@@ -1,7 +1,45 @@
 package service;
 
+import java.math.BigDecimal;
 
-public class MarginAccountService {
+import pojo.MarginAccount;
+import repository.TradeAccountRepository;
 
+public class MarginAccountService implements TradeAccountService {
 
+  private TradeAccountRepository tradeAccountRepository;
+
+  public MarginAccountService(TradeAccountRepository repository) {
+    this.tradeAccountRepository = repository;
+  }
+
+  public void createMarginAccount(MarginAccount account) {
+    this.tradeAccountRepository.createTradeAccount(account);
+  }
+
+  public MarginAccount retrieveMarginAccount(String id) {
+    return (MarginAccount) this.tradeAccountRepository.retrieveTradeAccount(id);
+  }
+
+  public void updateMarginAccount(MarginAccount account) {
+    this.tradeAccountRepository.updateTradeAccount(account);
+  }
+
+  public void deleteMarginAccount(String id) {
+    this.tradeAccountRepository.deleteTradeAccount(id);
+  }
+
+  @Override
+  public void deposit(String id, BigDecimal amount) {
+    MarginAccount marginAccount = retrieveMarginAccount(id);
+    marginAccount.setMargin(marginAccount.getMargin().add(amount));
+    this.tradeAccountRepository.updateTradeAccount(marginAccount);
+  }
+
+  @Override
+  public void withdraw(String id, BigDecimal amount) {
+    MarginAccount marginAccount = retrieveMarginAccount(id);
+    marginAccount.setMargin(marginAccount.getMargin().subtract(amount));
+    this.tradeAccountRepository.updateTradeAccount(marginAccount);
+  }
 }
