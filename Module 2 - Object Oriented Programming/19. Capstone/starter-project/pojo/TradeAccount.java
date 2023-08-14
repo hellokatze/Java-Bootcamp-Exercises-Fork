e84ea0387e169498f2a2ac9c2e9e53a6ada21e11
Katharine Lin <katharine.exe@gmail.com>
2023-08-14 12:58:35 +0200

    add getter and setter for TradeAccount class

--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/TradeAccount.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/TradeAccount.java
@@ -2,5 +2,13 @@ package pojo;
 
 public abstract class TradeAccount {
 
+  private String id;
 
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
 }
