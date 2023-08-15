461f457be5af9d6aac69860148149d2c9d4255cd
Katharine Lin <katharine.exe@gmail.com>
2023-08-15 10:14:18 +0200

    create thread and class that implements Runnable interface

--- Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/intro-to-concurrency/MultiThread.java
+++ Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/intro-to-concurrency/MultiThread.java
@@ -1,9 +1,18 @@
 public class MultiThread {
 
+    static class LongTask implements Runnable {
+      @Override
+      public void run() {
+        longTask();
+      }
+    }
+
     public static void main(String[] args) {
 
+      LongTask runnable = new LongTask(); // create a new object of the LongTask class
+      Thread thread2 = new Thread(runnable); // create a new thread where longTask() will be executed.
         System.out.println("Doing other work on main thread");
-        
+      thread2.start(); // this starts a new thread and executes its task by invoking its run method.
     }
 
     public static void longTask() {
