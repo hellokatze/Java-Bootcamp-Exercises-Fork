5708d7803e38afe7f5b820bf823408b19fef2336
Katharine Lin <katharine.exe@gmail.com>
2023-08-15 10:16:30 +0200

    refactor with anonymous class

--- Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/intro-to-concurrency/MultiThread.java
+++ Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/intro-to-concurrency/MultiThread.java
@@ -1,30 +1,36 @@
 public class MultiThread {
 
-    static class LongTask implements Runnable {
-      @Override
-      public void run() {
-        longTask();
-      }
-    }
+  // static class LongTask implements Runnable {
+  //   @Override
+  //   public void run() {
+  //     longTask();
+  //   }
+  // }
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      LongTask runnable = new LongTask(); // create a new object of the LongTask class
+      Runnable runnable = new Runnable() {
+      // This makes an anonymous class so you don't have to declare it above. But you must override its methods:
+        @Override
+        public void run() {
+          longTask();
+        }
+      // LongTask runnable = new LongTask(); // create a new object of the LongTask class
       Thread thread2 = new Thread(runnable); // create a new thread where longTask() will be executed.
         System.out.println("Doing other work on main thread");
       thread2.start(); // this starts a new thread and executes its task by invoking its run method.
     }
 
-    public static void longTask() {
-        long t= System.currentTimeMillis();
-        long end = t+3000;
+  public static void longTask() {
+    long t = System.currentTimeMillis();
+    long end = t + 3000;
 
-        while(true) {
-            if (System.currentTimeMillis() == end) {
-                System.out.println("Finished long task");
-                break;
-            }
-        }
+    while (true) {
+      if (System.currentTimeMillis() == end) {
+        System.out.println("Finished long task");
+        break;
+      }
     }
+  }
 
 }
