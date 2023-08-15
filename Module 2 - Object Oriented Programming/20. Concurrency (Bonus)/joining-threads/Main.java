39f1b6986441f713d2c9c754bbf1267a6def09d8
Katharine Lin <katharine.exe@gmail.com>
2023-08-15 13:12:19 +0200

    join thread

--- Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/joining-threads/Main.java
+++ Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/joining-threads/Main.java
@@ -1,24 +1,56 @@
+import java.util.Scanner;
+
 public class Main {
 
     static final double TARGET = 0.5;
     static final double PRECISION = 0.000000001;
 
+    static double result = 0;
+
     public static void main(String[] args) {
 
         //call generateNumber here...
+        Runnable runnable = () -> result = generateNumber();
+        Thread thread = new Thread(runnable);
+        thread.start();
         //calculate precision level here...
-        System.out.println("The computer returned a value of: " + <result>);
-        System.out.println("The value was generated to a precision of : " + <precision>);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter a name to generate a number: ");
+        scan.next();
+        scan.close();
+        try {
+          thread.join();
+        } catch (InterruptedException e) {
+          System.out.println(e.getMessage());
+        }
+        double precision = difference(result);
+
+        System.out.println("The computer returned a value of: " + result);
+        System.out.println("The value was generated to a precision of : " + precision);
     }
 
     /**
      * Function name: generateNumber
      * @return double
-     * 
+     *
      * Inside the function:
      *   1. Generates a number close to the TARGET to a precision of PRECISION.
-     * 
+     *
      */
 
+    public static double generateNumber() {
+      double number = Math.random();
+      double difference = difference(number);
+      while (difference > PRECISION) {
+        number = Math.random();
+        difference = difference(number);
+      }
+      return number;
+    }
+
+    public static double difference(double number) {
+      return Math.abs(TARGET - number);
+    }
 
 }
