8f150908071305ab58a2dc7730833c4a0eadf964
Katharine Lin <katharine.exe@gmail.com>
2023-08-15 10:57:12 +0200

    create average and totalAverage methods

--- Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/Workbooks/Workbook 11.1/Main.java
+++ Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/Workbooks/Workbook 11.1/Main.java
@@ -1,4 +1,6 @@
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -8,7 +10,7 @@ public class Main {
     static final String SALES = "data/sales.csv"; //Use backslash Windows users
 
     public static void main(String[] args) {
-        
+
         try {
             Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(SALES).toURI());
             //calculate average sales of "Furniture" here
@@ -21,12 +23,25 @@ public class Main {
         }
     }
 
+    public static Double average(Path path, String category) throws IOException {
+      try {
+        return Files.lines(path)
+          .skip(1)
+          .map((line) -> line.split(","))
+          .filter((values) -> values[0].equals(category))
+          .mapToDouble((values) -> Double.valueOf(values[1]) * Double.valueOf(values[2]))
+          .average().getAsDouble();
+      } catch (IOException e) {
+        System.out.println(e.getMessage());
+        return 0.0;
+      }
+    }
     /**
      * Function name: average
      * @param path (Path)
      * @param category (String)
      * @return Double
-     * 
+     *
      * Inside the function:
      *   1. Runs through every line from the CSV file as a stream.
      *   2. Maps every element in the stream to an array of three String values.
@@ -34,16 +49,27 @@ public class Main {
      *   4. Maps every element in the stream to a double (price * quantity).
      *   5. Applies the terminal operation average.
      *   6. Returns the average as double.
-     * 
+     *
      */
 
 
-
+    public static Double totalAverage(Path path) {
+      try {
+        return Files.lines(path)
+          .skip(1)
+          .map((line) -> line.split(","))
+          .mapToDouble((values) -> Double.valueOf(values[1]) * Double.valueOf(values[2]))
+          .average().getAsDouble();
+      } catch (IOException e) {
+        System.out.println(e.getMessage());
+        return 0.0;
+      }
+    }
     /**
      * Function name: totalAverage
      * @param path (Path)
      * @return Double
-     * 
+     *
      * Inside the function:
      *   1. Runs through every line from the CSV file as a stream.
      *   2. Maps every element in the stream to an array of three values.
@@ -51,6 +77,6 @@ public class Main {
      *   4. Applies the terminal operation average.
      *   5. Returns the average as double.
      */
-  
+
 
 }
