bdfb8c3906cf92e0f8e8031d147e11d7c10e6b67
Katharine Lin <katharine.exe@gmail.com>
2023-08-15 11:08:33 +0200

    catch IOException in main

--- Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/Workbooks/Workbook 11.1/Main.java
+++ Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/Workbooks/Workbook 11.1/Main.java
@@ -3,80 +3,96 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.Scanner;
 
 public class Main {
 
-    static final String SALES = "data/sales.csv"; //Use backslash Windows users
+  static final String SALES = "data/sales.csv"; // Use backslash Windows users
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        try {
-            Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(SALES).toURI());
-            //calculate average sales of "Furniture" here
-            //calculate average sales of "Technology" here
-            //calculate average sales of "Office Supplies" here
-            //calculate total average of sales here
-
-        } catch (URISyntaxException e) {
-            System.out.println(e.getMessage());
-        }
+    try {
+      Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(SALES).toURI());
+      average(path, "Furniture");
+      average(path, "Technology");
+      average(path, "Office Supplies");
+      totalAverage(path);
+    } catch (URISyntaxException e) {
+      System.out.println(e.getMessage());
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
     }
 
-    public static Double average(Path path, String category) throws IOException {
-      try {
-        return Files.lines(path)
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Please enter your name to access the Global Superstore dataset: ");
+    String name = scan.nextLine();
+    System.out.println("Access Denied. We apologize for the inconvenience. Have a good day " + name + ".");
+    scan.close();
+
+  }
+
+  public static Double average(Path path, String category) throws IOException {
+    try {
+      return Files.lines(path)
           .skip(1)
           .map((line) -> line.split(","))
           .filter((values) -> values[0].equals(category))
           .mapToDouble((values) -> Double.valueOf(values[1]) * Double.valueOf(values[2]))
           .average().getAsDouble();
-      } catch (IOException e) {
-        System.out.println(e.getMessage());
-        return 0.0;
-      }
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      return 0.0;
     }
-    /**
-     * Function name: average
-     * @param path (Path)
-     * @param category (String)
-     * @return Double
-     *
-     * Inside the function:
-     *   1. Runs through every line from the CSV file as a stream.
-     *   2. Maps every element in the stream to an array of three String values.
-     *   3. Filters every value by the @param category
-     *   4. Maps every element in the stream to a double (price * quantity).
-     *   5. Applies the terminal operation average.
-     *   6. Returns the average as double.
-     *
-     */
+  }
 
 
-    public static Double totalAverage(Path path) {
-      try {
-        return Files.lines(path)
-          .skip(1)
-          .map((line) -> line.split(","))
-          .mapToDouble((values) -> Double.valueOf(values[1]) * Double.valueOf(values[2]))
-          .average().getAsDouble();
-      } catch (IOException e) {
-        System.out.println(e.getMessage());
-        return 0.0;
-      }
+  public static Double totalAverage(Path path) {
+    try {
+      return Files.lines(path)
+      .skip(1)
+      .map((line) -> line.split(","))
+      .mapToDouble((values) -> Double.valueOf(values[1]) * Double.valueOf(values[2]))
+      .average().getAsDouble();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      return 0.0;
     }
-    /**
-     * Function name: totalAverage
-     * @param path (Path)
-     * @return Double
-     *
-     * Inside the function:
-     *   1. Runs through every line from the CSV file as a stream.
-     *   2. Maps every element in the stream to an array of three values.
-     *   3. Maps every element in the stream to a double: (price * quantity).
-     *   4. Applies the terminal operation average.
-     *   5. Returns the average as double.
-     */
+  }
 
+
+
+  /**
+   * Function name: average
+   *
+   * @param path     (Path)
+   * @param category (String)
+   * @return Double
+   *
+   *         Inside the function:
+   *         1. Runs through every line from the CSV file as a stream.
+   *         2. Maps every element in the stream to an array of three String
+   *         values.
+   *         3. Filters every value by the @param category
+   *         4. Maps every element in the stream to a double (price * quantity).
+   *         5. Applies the terminal operation average.
+   *         6. Returns the average as double.
+   *
+   */
+
+
+
+  /**
+   * Function name: totalAverage
+   *
+   * @param path (Path)
+   * @return Double
+   *
+   *         Inside the function:
+   *         1. Runs through every line from the CSV file as a stream.
+   *         2. Maps every element in the stream to an array of three values.
+   *         3. Maps every element in the stream to a double: (price * quantity).
+   *         4. Applies the terminal operation average.
+   *         5. Returns the average as double.
+   */
 
 }
