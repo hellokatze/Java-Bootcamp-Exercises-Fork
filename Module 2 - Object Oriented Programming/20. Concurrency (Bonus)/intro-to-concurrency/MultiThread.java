9394876536d53c0c2c357642e6fb017dc6ea0c32
Katharine Lin <katharine.exe@gmail.com>
2023-08-15 10:21:02 +0200

    refactor using lambda expression

--- Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/intro-to-concurrency/MultiThread.java
+++ Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/intro-to-concurrency/MultiThread.java
@@ -8,17 +8,21 @@ public class MultiThread {
   // }
 
   public static void main(String[] args) {
-
-      Runnable runnable = new Runnable() {
-      // This makes an anonymous class so you don't have to declare it above. But you must override its methods:
-        @Override
-        public void run() {
-          longTask();
-        }
-      // LongTask runnable = new LongTask(); // create a new object of the LongTask class
-      Thread thread2 = new Thread(runnable); // create a new thread where longTask() will be executed.
-        System.out.println("Doing other work on main thread");
-      thread2.start(); // this starts a new thread and executes its task by invoking its run method.
+      // when possible, avoid anonymous classes and use a lambda expression instead.
+      Thread thread2 = new Thread(() -> longTask());
+      thread2.start();
+      System.out.println("Doing other work on main thread");
+      
+      // Runnable runnable = new Runnable() {
+      // // This makes an anonymous class so you don't have to declare it above. But you must override its methods:
+      //   @Override
+      //   public void run() {
+      //     longTask();
+      //   }
+      // };
+      // // LongTask runnable = new LongTask(); // create a new object of the LongTask class
+      // Thread thread2 = new Thread(runnable); // create a new thread where longTask() will be executed.
+      // thread2.start(); // this starts a new thread and executes its task by invoking its run method.
     }
 
   public static void longTask() {
