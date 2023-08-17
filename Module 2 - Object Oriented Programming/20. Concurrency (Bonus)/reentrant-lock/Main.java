ea711184633f3c6d6754fc63f009c52792579abc
Katharine Lin <katharine.exe@gmail.com>
2023-08-17 16:55:52 +0200

    refactor using atomic type

--- Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/reentrant-lock/Main.java
+++ Module 2 - Object Oriented Programming/20. Concurrency (Bonus)/reentrant-lock/Main.java
@@ -1,21 +1,21 @@
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
-  static int counter = 0;
+  static AtomicInteger counter = new AtomicInteger(0); // this is a class type, not a primitive type, anymore.
 
   public static void main(String[] args) {
 
     CountDownLatch latch = new CountDownLatch(2);
-    ReentrantLock lock = new ReentrantLock();
 
     int numThreads = Runtime.getRuntime().availableProcessors();
     ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-    executor.submit(() -> task(lock, latch));
-    executor.submit(() -> task(lock, latch));
+    executor.submit(() -> task(latch));
+    executor.submit(() -> task(latch));
     // Thread thread1 = new Thread(() -> task(lock, latch));
     // Thread thread2 = new Thread(() -> task(lock, latch));
 
@@ -33,12 +33,10 @@ public class Main {
     System.out.println(counter);
   }
 
-  public static void task(ReentrantLock lock, CountDownLatch latch) {
+  public static void task(CountDownLatch latch) {
     // some really long operations that need to run in the background...
     for (int i = 0; i < 10000; i++) {
-      lock.lock();
-      counter++; // there happens to be a line that updates the counter variable.
-      lock.unlock();
+      counter.addAndGet(1); // there happens to be a line that updates the counter variable.
     }
     latch.countDown();
     // more really long operations...
